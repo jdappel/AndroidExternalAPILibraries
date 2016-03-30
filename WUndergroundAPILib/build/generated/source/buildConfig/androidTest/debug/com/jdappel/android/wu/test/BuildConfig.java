14efca1bc428f467685736129deb3c37eca370bf
Jeremy Appel <jappel@kenzanmedia.com>
2016-03-30 15:04:43 -0400

    initial commit

---
+++ WUndergroundAPILib/build/generated/source/buildConfig/androidTest/debug/com/jdappel/android/wu/test/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jdappel.android.wu.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.jdappel.android.wu.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Fields from build type: debug
+  public static final String ARTIFACT_ID = "wunderground-lib";
+  public static final long BUILD_TIME = 1459364186725L;
+  public static final String GROUP = "com.jdappel.android";
+}
